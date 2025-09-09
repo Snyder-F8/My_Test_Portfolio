b7ed2c12444766087f7af36ed3020243499ab649
Snyder-F8 <fridahnzomo857@gmail.com>
2025-09-09 11:13:07 +0300

    Changed my portfolio details

---
+++ script.js
@@ -0,0 +1,10 @@
+function shopNow() {
+  alert("Welcome to Glow Beauty! Redirecting to shop...");
+  window.location.href = "#products";
+}
+function showAlert(message) {
+  alert(message);
+}
+
+// Example usage:
+// showAlert("This is a custom alert message!");
